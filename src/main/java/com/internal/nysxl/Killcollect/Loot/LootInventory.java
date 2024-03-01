a0ca75bbab75cc4fc391b39fb25511cae6983821
Nysxl <109190345+Nysxl@users.noreply.github.com>
2024-03-02 07:43:01 +1100

    V1.0.1

--- src/main/java/com/internal/nysxl/Killcollect/Loot/LootInventory.java
+++ src/main/java/com/internal/nysxl/Killcollect/Loot/LootInventory.java
@@ -1,20 +1,50 @@
 package com.internal.nysxl.Killcollect.Loot;
 
-import com.internal.nysxl.NysxlUtilities.GUIManager.DynamicGUI;
+import com.internal.nysxl.Killcollect.main;
 import com.internal.nysxl.NysxlUtilities.GUIManager.DynamicListGUI;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 public class LootInventory {
 
-    DynamicListGUI gui = new DynamicListGUI("Spoils", 54);
+    /**
+     * Constructs a LootInventory instance and displays it to the player.
+     * If the player has no loot, a message is sent to them. Otherwise,
+     * the loot is displayed in a GUI.
+     *
+     * @param player The player for whom the loot inventory is to be displayed.
+     */
+    public LootInventory(Player player) {
+        List<Loot> playerLoot = main.playerLoot.computeIfAbsent(player, k -> new java.util.ArrayList<>());
 
-    public LootInventory lootInventory(Player player){
-        gui.ac
+        if(playerLoot.isEmpty()) {
+            player.sendMessage("You don't currently have any loot.");
+            return;
+        }
+
+        DynamicListGUI gui = new DynamicListGUI("Spoils", 54);
+        populateGUIWithLoot(gui, playerLoot);
+        gui.open(player);
     }
 
-
-
-
+    /**
+     * Populates the given GUI with the player's loot items. Each item in the loot list
+     * is added to the GUI with an associated action that opens the inventory when clicked.
+     *
+     * @param gui      The GUI to be populated with loot items.
+     * @param lootList The list of loot items to add to the GUI.
+     */
+    private void populateGUIWithLoot(DynamicListGUI gui, List<Loot> lootList) {
+        for(Loot loot : lootList) {
+            Consumer<Player> consumer = loot::openInv;
+            ItemStack slotItem = loot.getSlotItem() != null ? loot.getSlotItem() : new ItemStack(Material.PLAYER_HEAD);
+            gui.addItem(slotItem, consumer);
+        }
+    }
 
 
 
--- src/main/java/com/internal/nysxl/Killcollect/main.java
+++ src/main/java/com/internal/nysxl/Killcollect/main.java
@@ -2,7 +2,9 @@ package com.internal.nysxl.Killcollect;
 
 import com.internal.nysxl.Killcollect.Listener.EntityKill;
 import com.internal.nysxl.Killcollect.Listener.InventoryEvents;
+import com.internal.nysxl.Killcollect.Listener.LootCommand;
 import com.internal.nysxl.Killcollect.Loot.Loot;
+import com.internal.nysxl.NysxlUtilities.GUIManager.GUIManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -18,6 +20,8 @@ public class main extends JavaPlugin {
     public void onEnable(){
         Bukkit.getPluginManager().registerEvents(new EntityKill(), this);
         Bukkit.getPluginManager().registerEvents(new InventoryEvents(), this);
+        Bukkit.getPluginManager().registerEvents(new GUIManager(this), this);
+        this.getCommand("openloot").setExecutor(new LootCommand());
     }
 
 
